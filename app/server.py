52b39abeda3250fe86189d63291705e4b027ea01
Andrej Babic <andbab@gmail.com>
2013-02-01 13:10:34 +0100

    Bugfix: didn't want to run on AppFog

--- app/server.py
+++ app/server.py
@@ -12,25 +12,24 @@ class FlaskServer(object):
         config.load_config(config_file)
         self.config = config
         self.debug = self.config['server'].as_bool('debug')
-        self.flask_app = self.create_app(config, 'app')
+        self.create_app(config, 'app')
         self.register_blueprints()
         self.configure_webassets()
 
     def get_app(self):
-        return self.flask_app
+        return self.app
 
     def create_app(self, config, app_name):
-        app = Flask(app_name)
-        app.config.update(config[app_name].dict())
-        return app
+        self.app = Flask(app_name)
+        self.app.config.update(config[app_name].dict())
 
     def register_blueprints(self):
-        self.flask_app.register_blueprint(bp, url_prefix='')
+        self.app.register_blueprint(bp, url_prefix='')
 
     def configure_webassets(self):
-        assets = Environment(self.flask_app)
-        assets.init_app(self.flask_app)
+        assets = Environment(self.app)
+        assets.init_app(self.app)
         assets.debug = self.debug
 
     def run(self):
-        self.flask_app.run(debug=self.debug, use_reloader=self.debug, use_debugger=self.debug)
+        self.app.run(debug=self.debug, use_reloader=self.debug, use_debugger=self.debug)
