9f66c8b63e35f0b746ec601c172f14d13b499e94
Andrej Babic <andbab@gmail.com>
2013-08-01 20:10:04 +0200

    Feature: updated to latest Flask

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     version=VERSION,
     author='jdi',
     install_requires=[
-        'flask==0.10',
+        'flask==0.10.1',
         'jinja2==2.7',
         'flask-assets==0.8',
         'flask-debugtoolbar',
